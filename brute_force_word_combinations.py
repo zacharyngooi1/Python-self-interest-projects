79297abad071e71d3a3a38f879c0ad0b0235f670
z5228846 <z.ngooi@student.unsw.edu.au>
2020-09-01 18:37:38 +1000

    Created basic brute forcer for dictionary

---
+++ brute_force_word_combinations.py
@@ -0,0 +1,42 @@
+from nltk.corpus import words
+import itertools
+import time
+
+
+#Brute forcing takes a long time so i will try to iterate through common word sin the dictionary and concatente letters and symbols after
+
+Possible_letters = ("1234567890-_.!@#$%^&*()+=")
+
+word_list = words.words()
+new = []
+query = []
+print("Please enter the first letter alphabets of the password to be guessed:")
+
+while True:
+    poss = input()
+    if (poss == ""):
+        break
+    else:
+        query.append(poss)
+
+for finder in word_list:
+    for al in query:
+        if finder[0] == al: 
+            new.append(finder)
+
+password = "maximus1234"
+string_combi = ""
+#236736
+print("Cracking.....")
+time_start = time.perf_counter()
+for x in range(25):
+    produced_pass = itertools.product(Possible_letters, repeat=x)
+    for b in produced_pass:
+        b = list(b)
+        for word in new:
+            new_list = [word] + b
+            if string_combi.join(new_list) == password:
+                time_end = time.perf_counter()
+                time_taken = time_end - time_start
+                print("Password cracked!! Your password is: " + string_combi.join(new_list) + ", taking " + str(time_taken) + " seconds")
+                exit()
